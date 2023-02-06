c5da0189b51fa4c73e10ecc7a7626366a6187f02
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2023-02-06 12:37:20 +0100

    12:37
    script för rörliga plattformar

--- Assets/Bengt/PlayerMovement.cs
+++ Assets/Bengt/PlayerMovement.cs
@@ -27,8 +27,10 @@ public class PlayerMovement : MonoBehaviour{
         Vector2 corner1 = new Vector2(min.x, min.y - .1f);
         Vector2 corner2 = new Vector2(min.x, min.y - .2f);
         Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
-        bool grounded = false;
+        bool grounded = false; //changes to true if the player is on the ground and majes them 
+        //able to jump.
         
+        //detects if the player is on the ground or in the air.
         if (hit != null) {
             grounded = true;
         }
@@ -37,9 +39,21 @@ public class PlayerMovement : MonoBehaviour{
         if (grounded && Input.GetKeyDown(KeyCode.Space)) {
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        //checks if the platform is moving or not.
+        MovingPlatforms platform = null;
+        if (platform != null){
+            platform = hit.GetComponent<MovingPlatforms>(); 
+        }
+        //either attach the player to platform or clear transform.parent.
+        if (platform != null) {
+            transform.parent = platform.transform;
+        }
+        else {
+            transform.parent = null;
+        }
+        _anim.SetFloat("speed", Mathf.Abs(deltaX)); //speed greater than zero.
         if (!Mathf.Approximately(deltaX, 0)){
-            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1); //when moving scale postive or negative 1 to face left or right.
         }
     }
 }
--- Assets/MovingPlatforms.cs
+++ Assets/MovingPlatforms.cs
@@ -18,11 +18,10 @@ public class MovingPlatforms : MonoBehaviour {
     void Update() {
         _trackPercent += _direction * speed * Time.deltaTime;
         float x = (finishPos.x - _StartPos.x) * _trackPercent + _StartPos.x;
-        float y = (finishPos.y - _StartPos.x) * _trackPercent + _StartPos.x;
-        transform.position = new Vector3(x, y, _StartPos.z);
+        transform.position = new Vector3(x, _StartPos.y, _StartPos.z);
 
-        if ((_direction == 1 && _trackPercent > 0.9f) || (_direction == -1
-                && _trackPercent < 0.1f)) {
+        if ((_direction == 1 && _trackPercent > .9f) || (_direction == -1
+                && _trackPercent < .1f)) {
             _direction *= -1;
         }
     }
